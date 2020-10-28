fb39b006ab9711021cfd2b672758cf312dd4a9b9
Liujg <liujiguijava@163.com>
2020-10-28 08:07:04 +0800

    修改执行间隔时间为3S

--- src/main/java/com/apple/iphone/scheduling/TimedTask.java
+++ src/main/java/com/apple/iphone/scheduling/TimedTask.java
@@ -3,7 +3,6 @@ package com.apple.iphone.scheduling;
 import com.apple.iphone.service.InventoryInquiryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
 
 /**
